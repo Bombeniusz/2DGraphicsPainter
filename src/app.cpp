c25f54d3a0077424641fdbc42f445f81d976ede9
Marcin Caban <gmcabanpl@gmail.com>
2025-11-05 10:11:24 +0100

    Added color window, color picker, Viewport color window, color picking and painting viewport window is working.

--- src/app.cpp
+++ src/app.cpp
@@ -8,6 +8,7 @@
 #define DIRECTINPUT_VERSION 0x0800
 #include <dinput.h>
 #include <tchar.h>
+#include <vector>
 
 // Data
 static ID3D11Device* g_pd3dDevice = NULL;
@@ -97,6 +98,8 @@ int main(int, char**)
     bool show_demo_window = true;
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    ImVec4 sliderColor = ImVec4(0.55f, 0.75f, 0.60f, 1.00f);
+    ImVec4 viewPortBackgroundColor = ImVec4(0.55f, 0.75f, 0.60f, 1.00f);
 
     // Main loop
     MSG msg;
@@ -121,27 +124,56 @@ int main(int, char**)
         ImGui::NewFrame();
 
         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        //if (show_demo_window)
+            //ImGui::ShowDemoWindow(&show_demo_window);
 
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
         {
-            static float f = 0.0f;
-            static int counter = 0;
+            //static float f = 0.0f;
+            //static int counter = 0;
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+            ImGui::Begin("Colors");                          // Create a window called "Hello, world!" and append into it.
 
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
+            //ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            //ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("ViewPort", &show_another_window);
 
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+           /* ImGui::SliderFloat("float", &f, 0.0f, 1.0f);*/            // Edit 1 float using a slider from 0.0f to 1.0f
+            //ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+            //ImGui::ColorEdit3("ViewPortBackgroundColor", (float*)&sliderColor); // Edit 3 floats representing a color
+            //ImGui::ColorPicker4("Pick Color", (float*)&sliderColor);
+            ImGui::PushItemWidth(300);
+            ImGui::ColorPicker4("Viewport Color", (float*)&sliderColor);
+            ImGui::PopItemWidth();
 
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+            ImGui::Text("SomeText");
+            /*if (ImGui::IsMouseClicked(ImGuiMouseButton_Left))
+            {
+
+
+                if (ImGui::IsItemHovered())
+                {
+                    viewPortBackgroundColor = sliderColor;
+                }
+
+                if (ImGui::IsWindowHovered())
+                {
+                    viewPortBackgroundColor = sliderColor;
+                }
+            }*/
+
+            //ImGui::PushStyleColor(ImGuiCol_WindowBg, viewPortBackgroundColor);
+
+            //ImGui::Begin("Custom Background Window");
+
+            //ImGui::End();
+
+            //ImGui::PopStyleColor();
+
+            //if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            //    counter++;
+            //ImGui::SameLine();
+            //ImGui::Text("counter = %d", counter);
 
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             ImGui::End();
@@ -150,11 +182,24 @@ int main(int, char**)
         // 3. Show another simple window.
         if (show_another_window)
         {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
+
+            ImGui::PushStyleColor(ImGuiCol_WindowBg, viewPortBackgroundColor);
+            ImGui::Begin("Viewport", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            // must be within begin to work at least this version
+            if (ImGui::IsWindowHovered())
+            {
+                //ImGui::Text("Window Hovered!");
+                if (ImGui::IsMouseClicked(ImGuiMouseButton_Left))
+                {
+                    viewPortBackgroundColor = sliderColor;
+                }
+            }
+
+            ImGui::Text("Hello from viewport!");
             if (ImGui::Button("Close Me"))
                 show_another_window = false;
             ImGui::End();
+            ImGui::PopStyleColor();
         }
 
         // Rendering
